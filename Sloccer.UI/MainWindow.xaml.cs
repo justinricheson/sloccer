400fc434229dea33e2d73eccb7b0a33af3b7baf5
Justin Richeson <justinricheson@gmail.com>
2015-04-12 14:00:24 -0500

    Project structure

---
+++ Sloccer.UI/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Sloccer.UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
